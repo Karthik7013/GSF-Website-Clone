c59744608557506c44d6208d655d0de65dc5ad9c
Karthik Tumala <113322353+Karthik7013@users.noreply.github.com>
2023-09-16 15:16:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let x = document.getElementsByTagName("body");
+x[0].onload = () => {
+    let one = document.getElementById("one");
+    one.classList.add("show");
+    let two = document.getElementById("two");
+    two.classList.add("show");
+
+
+
+}
